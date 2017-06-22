6f23c1cd1ad45324206b0503bb67b75a8f25b40a
Colin Dean <colin.dean@arcadiasolutions.com>
2017-06-22 16:20:37 -0400

    Drops brew-cask installation since it's already installed with Homebrew

--- setup-caskroom.sh
+++ setup-caskroom.sh
@@ -6,15 +6,17 @@ command_exists "brew"
 brew_exists=$?
 if [[ $brew_exists -ne 0 ]]; then
   echo >&2 "brew isn't available, installing Homebrew."
-  install_homebrew 
+  install_homebrew
 fi
 
-command_exists "brew-cask"
-brewcask_exists=$?
-if [[ $brewcask_exists -ne 0 ]]; then
-  echo >&2 "brew-cask isn't available, installing Caskroom."
-  brew install caskroom/cask/brew-cask
-fi
+# this is probably no longer necessary since Homebrew ships with Caskroom already
+# installed as of at least June 2017. Leave this here until the next time this is used.
+#command_exists "brew-cask"
+#brewcask_exists=$?
+#if [[ $brewcask_exists -ne 0 ]]; then
+#  echo >&2 "brew-cask isn't available, installing Caskroom."
+#  brew install caskroom/cask/brew-cask
+#fi
 
 install_packages "caskroom.list" 'brew cask install %PACKAGE%'
 
