ef1436cbad54692df2796ae25531f335202f60f8
Colin Dean <colin.dean@arcadiasolutions.com>
2017-07-29 12:25:12 -0400

    Adds anycomplete to Hammerspoon config
    Thanks, @nathancahill!

--- dotfiles/hammerspoon/init.lua
+++ dotfiles/hammerspoon/init.lua
@@ -4,5 +4,8 @@ package.path = package.path .. ";" .. os.getenv("HOME") .. "/.hammerspoon/?.lua"
 
 require "window_mgmt"
 
+local anycomplete = require "anycomplete"
+anycomplete.registerDefaultBindings()
+
 local alert = require "hs.alert"
 alert.show("🔨 ⏰")
