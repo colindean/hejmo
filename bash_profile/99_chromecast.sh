e51f80989923031019cc4e7b5b77d0c5278033b2
Colin Dean <git@cad.cx>
2018-07-29 22:00:31 -0400

    Adds some Chromecasting tools

---
+++ bash_profile/99_chromecast.sh
@@ -0,0 +1,13 @@
+function ccyt {
+  curl -H "Content-Type: application/json" \
+    http://"Basement TV.local":8008/apps/YouTube \
+    -X POST \
+    -d "v=$1";
+}
+
+function ytsearch() {
+  curl -s "https://www.youtube.com/results?search_query=$@" | \
+    grep -o 'watch?v=[^"]*"[^>]*title="[^"]*' | \
+    sed -e 's/^watch\?v=\([^"]*\)".*title="\(.*\)/\1 \2/g'
+}
+
