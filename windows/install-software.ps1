9bf5717357344c7a18ab60a60fddf7e3246fd5b5
Colin Dean <colindean@users.noreply.github.com>
2019-01-30 14:14:57 -0500

    Removes development-only tools from Windows software list

--- windows/install-software.ps1
+++ windows/install-software.ps1
@@ -1,1 +1,2 @@
-choco install --yes discord googlechrome autohotkey firefox 7zip vlc steam steam-cleaner goggalaxy windirstat battle.net dropbox duplicati filezilla lastpass launchy obs-studio putty deluge adobereader git sysinternals atom procexp chocolateygui aquasnap
+choco install --yes discord googlechrome autohotkey firefox 7zip vlc steam steam-cleaner goggalaxy windirstat battle.net dropbox duplicati filezilla lastpass launchy obs-studio deluge adobereader sysinternals atom procexp chocolateygui aquasnap
+
