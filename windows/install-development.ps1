01c821a885dba97733a12304ea4525f8ec8b7101
Colin Dean <colindean@users.noreply.github.com>
2019-01-30 14:30:45 -0500

    Adds a few other development tools to Windows config

--- windows/install-development.ps1
+++ windows/install-development.ps1
@@ -1,7 +1,6 @@
+# Development software
 scoop bucket add extras
-
-scoop bucket add extras
-scoop install alacritty extras/vcredist2017 putty git 
+scoop install alacritty extras/vcredist2017 putty git bzip2 curl hub coreutils wget xmlstarlet
 
 # not needed after installation
 scoop uninstall vcredist2017
