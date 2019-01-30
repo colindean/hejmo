ec2b0b8961280019fdbd65b8b3954c6b3d96defa
Colin Dean <colindean@users.noreply.github.com>
2019-01-30 14:22:01 -0500

    Creates desktop-only Windows app list
    These are applications that only make sense if I'm setting up a new desktop for myself for work or play

---
+++ windows/install-desktop.ps1
@@ -0,0 +1,2 @@
+# programs that only make sense on a desktop workstation, not necessarily a (temporary) development environment
+choco install --yes vlc dropbox duplicati filezilla lastpass launchy deluge adobereader chocolateygui aquasnap
