53d567ca983ededa2563b149fa4749225cdc74f7
Colin Dean <colindean@users.noreply.github.com>
2019-01-30 14:39:26 -0500

    Create probably useless Windows installation script for my standard development stack

---
+++ windows/install-mystack.ps1
@@ -0,0 +1,7 @@
+# Install my standard development languages and frameworks
+# I'll probably never use this so it's probably out of date!
+scoop install sbt scala terraform ngrok neovim netcat wireshark gradle
+
+# install rustup to install rust instead of installing rust directly
+scoop install rustup
+rustup install stable
