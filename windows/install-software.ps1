91889565d9aa1ec6a2fed36cec43c2217345eb73
Colin Dean <colindean@users.noreply.github.com>
2019-01-30 14:22:29 -0500

    Reduces all-Windows-installation list to browsers, text editor, and process stuff

--- windows/install-software.ps1
+++ windows/install-software.ps1
@@ -1,2 +1,2 @@
-choco install --yes discord googlechrome autohotkey firefox 7zip vlc steam steam-cleaner goggalaxy windirstat battle.net dropbox duplicati filezilla lastpass launchy obs-studio deluge adobereader sysinternals atom procexp chocolateygui aquasnap
-
+# Software that should be installed on any Windows environment I touch
+choco install --yes googlechrome firefox brave 7zip windirstat sysinternals atom procexp 
