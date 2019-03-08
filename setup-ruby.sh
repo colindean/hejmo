aa3ee5a124b4a57ab134936566ce6d2bd9623b1e
Colin Dean <git@cad.cx>
2019-03-07 22:27:04 -0500

    Adds rvm installer
    I tried to make things work with ruby-install and chruby with Homebrew
    but got sudo errors… apparently ruby-install uses sudo to invoke brew
    and brew doesn't allow that anymore.

---
+++ setup-ruby.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# this will install or update RVM using
+# literally the steps from rvm.io
+ensure_brew() {
+  if [[ -z "$(command -v brew)" ]]; then
+    echo "Homebrew not found, can't install required dependencies."
+    exit 2
+  fi
+  true
+}
+if [[ -z "$(command -v curl)" ]]; then
+  echo "cURL not found. Are you even using a computer?"
+  echo "Let's try to install it with Homebrew but Homebrew relies on cURL already…"
+  ensure_brew && brew install curl
+fi
+if [[ -z "$(command -v gpg)" ]]; then
+  echo "GPG not found, installing it…"
+  ensure_brew && brew install gpg
+  if [[ -z "$(command -v gpg)" ]]; then
+    echo "GPG not available, cannot install RVM without being able to validate it."
+    exit 1
+  fi
+fi
+
+gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
+
