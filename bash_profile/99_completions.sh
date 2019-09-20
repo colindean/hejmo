343e38466cf4ad45316bb8773cd1305da6dff78d
Colin Dean <git@cad.cx>
2019-09-20 15:02:50 -0400

    Starts on completions for t with 'in' and 'sw'

--- bash_profile/99_completions.sh
+++ bash_profile/99_completions.sh
@@ -5,8 +5,14 @@ HOMEBREW_PREFIX="$(brew --prefix)"
 export HOMEBREW_COMPLETIONS_DIR="${HOMEBREW_PREFIX}/etc/bash_completion.d"
 export BASH_COMPLETION_COMPAT_DIR="${HOMEBREW_COMPLETIONS_DIR}"
 HOMEBREW_PROF_COMPLETION_SCRIPT="${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
-HOMEBREW_SOURCE_COMPLETIONS=yes
+export HOMEBREW_SOURCE_COMPLETIONS=yes
+# shellcheck source=/dev/null
 [[ -f "${HOMEBREW_PROF_COMPLETION_SCRIPT}" ]] && . "${HOMEBREW_PROF_COMPLETION_SCRIPT}"
 
 #autocomplete for g as well
 complete -o default -o nospace -F _git g
+
+#autocomplete for t
+# shellcheck source=../scripts/_t_completion
+[[ -n "$(command -v t)" ]] && [[ -n "$(command -v _t_completion)" ]] && source "$(command -v _t_completion)"
+
