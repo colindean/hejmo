dda8764f850764933cdd7ae7f9f2ff82eb005131
Colin Dean <git@cad.cx>
2020-06-18 13:56:56 -0400

    Adds Hammerspoon mash for toggling Zoom mute

--- dotfiles/hammerspoon/init.lua
+++ dotfiles/hammerspoon/init.lua
@@ -8,5 +8,22 @@ require "type"
 local anycomplete = require "anycomplete"
 anycomplete.registerDefaultBindings()
 
+local hotkeyerr, hotkey = pcall(function() return require "hs.hotkey" end)
+local alerterr, alert = pcall(function() return require "hs.alert" end)
+mash = {"cmd", "alt", "ctrl"}
+
+function toggle_zoom_mute()
+  local zoom = hs.application.find("zoom.us")
+  if not(zoom:findMenuItem("Unmute Audio")) then
+    zoom:selectMenuItem("Mute Audio")
+  elseif not(zoom:findMenuItem("Mute Audio")) then
+    zoom:selectMenuItem("Unmute Audio")
+  end
+end
+
+hotkey.bind(mash, "V", toggle_zoom_mute)
+
 local alert = require "hs.alert"
 alert.show("🔨 ⏰")
+
+
