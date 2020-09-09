4e9d45e9093d2f81bc228011c193d43fe5549962
Colin Dean <git@cad.cx>
2020-09-09 00:36:51 -0400

    Adds Linuxbrew to PATH in its default location
    and fixes a shellcheck warning on ~ vs $HOME

--- bash_profile/00_path.sh
+++ bash_profile/00_path.sh
@@ -1,16 +1,21 @@
+#!/usr/bin/env bash
 #export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_66.jdk/Contents/Home
 export JAVA_HOME=/Library/Java/Home
 function join { local IFS="$1"; shift; echo "$*"; }
 # Setup PATH
 MYPATH=()
 ## my scripts
-MYPATH+=('~/.bin')
+MYPATH+=("${HOME}/.bin")
 ## brew's python
 MYPATH+=('/usr/local/opt/python/libexec/bin')
 ## homebrew curl
 MYPATH+=('/usr/local/opt/curl/bin')
 ## homebrew
 MYPATH+=('/usr/local/bin:/usr/local/sbin')
+LINUXBREW_PATH="${LINUXBREW_PATH:-/home/linuxbrew/.linuxbrew}"
+[[ "Linux" == "$(uname -s)" ]] && \
+  [[ -d "${LINUXBREW_PATH}/bin" ]] && \
+  MYPATH+=("${LINUXBREW_PATH}/bin")
 ## java
 MYPATH+=("$JAVA_HOME/bin")
 ## rust
