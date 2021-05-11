314747f64f8fc6c01cce680812fcb7e917c2daed
Colin Dean <git@cad.cx>
2021-05-10 22:26:13 -0400

    Adds a function to disable iTerm integ when SSHing into a Mac

--- bash_profile/50_iterm_integration.sh
+++ bash_profile/50_iterm_integration.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 
-test -e "${HOME}/.iterm2_shell_integration.bash" && . "${HOME}/.iterm2_shell_integration.bash"
+ITERM_INTEGRATION_SCRIPT="${HOME}/.iterm2_shell_integration.bash"
+
+if [ -z "${NO_ITERM}" ] && [ -f "${ITERM_INTEGRATION_SCRIPT}" ] ; then
+  # shellcheck source=~/.iterm2_shell_integration.bash
+  . "${ITERM_INTEGRATION_SCRIPT}"
+fi
+
+disable-iterm-integration() {
+  local OUTPUT=""
+  IFS=';' read -ra CMDS <<< "${PROMPT_COMMAND}"
+  for i in "${CMD[@]}"; do
+    if [[ ! "${i}" =~ "__bp" ]]; then
+      OUTPUT="${OUTPUT};${i}"
+    fi
+  done
+
+  export PROMPT_COMMAND="${OUTPUT}"
+}
+
+if [ -n "${SSH_CONNECTION}" ]; then
+  echo "Connected via SSH, disabling iTerm integration." >&2
+  disable-iterm-integration
+fi
