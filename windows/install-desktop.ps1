0169ed1e3300e1e2995c309518bd972032643a15
Colin Dean <colindean@users.noreply.github.com>
2022-01-23 00:02:07 -0500

    Update install-desktop.ps1

--- windows/install-desktop.ps1
+++ windows/install-desktop.ps1
@@ -1,2 +1,4 @@
 # programs that only make sense on a desktop workstation, not necessarily a (temporary) development environment
-choco install --yes vlc dropbox duplicati filezilla lastpass launchy deluge adobereader chocolateygui aquasnap
+choco install --yes vlc dropbox duplicati filezilla launchy adobereader chocolateygui aquasnap windirstat transgui InkScape bitwarden
+
+scoop install wincompose ytmdesktop
