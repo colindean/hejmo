e9d58ed4ad5e5522d50d4ccda7645763285ff40d
Colin Dean <git@cad.cx>
2022-02-06 23:51:05 +0000

    Fixes java update-alternatives resolver

--- bash_profile/00_path.sh
+++ bash_profile/00_path.sh
@@ -33,12 +33,14 @@ eval "$("$(__determine_brew_path)/bin/brew" shellenv)"
 ## java
 if [[ "Linux" == "$(uname -s)" ]] && [[ -f /bin/update-alternatives ]]; then
   # update-alternatives gives ${JAVA_HOME}/jre/bin/java, so we gotta safely traverse upward
-  export JAVA_HOME="$(dirname $(dirname $(dirname $(/bin/update-alternatives --query java | grep "^Value" | cut -f 2 -d ' '))))"
+  if java_ua_output="$(/bin/update-alternatives --query java 2>/dev/null)"; then
+    export JAVA_HOME="$(dirname $(dirname $(dirname $( echo "${java_ua_output}" | grep "^Value" | cut -f 2 -d ' '))))"
+  fi
 elif [[ "Darwin" == "$(uname -s)" ]]; then
   # macOS manages Java smartly
   export JAVA_HOME=/Library/Java/Home
 else
-  echo "Could not determine JAVA_HOME"
+  echo "Could not determine JAVA_HOME for $(uname -s) $(command -v lsb_release >/dev/null && lsb_release -sd || true) "
 fi
 if [[ -n "${JAVA_HOME}" ]]; then
   MYPATH+=("$JAVA_HOME/bin")
