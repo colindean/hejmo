e1c0e2f125682ab0a60fdee622152840174cab88
Colin Dean <git@cad.cx>
2022-02-07 00:27:10 +0000

    Fixes rvm sourcing
    I guess that's why RVM was always yelling at me!

--- bash_profile/99_ruby.sh
+++ bash_profile/99_ruby.sh
@@ -3,14 +3,14 @@
 
 if [[ -z $NO_RVM ]]; then
   if [ -s "$HOME/.rvm/scripts/rvm" ]; then
-    "$HOME/.rvm/scripts/rvm" # Load RVM function
+    . "$HOME/.rvm/scripts/rvm" # Load RVM function
   fi
 fi
 
 if [[ -z $NO_CHRUBY ]]; then
   chruby_share="$(${BREW_PREFIX} chruby)/share/chruby"
   if [ -s "${chruby_share}/chruby.sh" ]; then
-    "${chruby_share}/chruby.sh"
+    . "${chruby_share}/chruby.sh"
   fi
   if [ -s "${chruby_share}/auto.sh" ]; then
     . "${chruby_share}/auto.sh"
