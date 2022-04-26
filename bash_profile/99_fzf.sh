76884623f8320bc8753905a97e054317f4df2110
Colin Dean <colin.dean@target.com>
2022-04-26 15:30:01 -0400

    Shellcheck and improve utility of fzf

--- bash_profile/99_fzf.sh
+++ bash_profile/99_fzf.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 # mostly copied from https://github.com/tednaleid/shared-zshrc/blob/master/zshrc_base#L265-L279
 
-[[ "$(uname -s)" == "Darwin" ]] && XARGS="gxargs" || XARGS="xargs"
-
 default_fzf_location=/usr/local/opt/fzf
-[[ -n "$(command -v brew)" ]] && BREW_FZF="$(${BREW_PREFIX} fzf)"
+if [[ -n "$(command -v brew)" ]]; then
+  BREW_FZF="$(${BREW_PREFIX} fzf)"
+fi
 FZF_SHELL="${BREW_FZF:-$default_fzf_location}/shell"
 
+XARGS="xargs"
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  if command -v gxargs > /dev/null; then
+    XARGS="gxargs"
+  else
+    >&2 echo "Trying to install GNU findutils to enable open_fzf support"
+    if [[ -n "${BREW_FZF}" ]]; then
+      brew install findutils
+      XARGS="gxargs"
+    fi
+  fi
+fi
+
+# shellcheck source=/dev/null
 if [[ -d "$FZF_SHELL" ]]; then
   source "${FZF_SHELL}/completion.bash" 2> /dev/null
   source "${FZF_SHELL}/key-bindings.bash"
@@ -28,10 +42,12 @@ export FZF_DEFAULT_OPTS="
   --height=80%
   --bind '?:toggle-preview'
 "
+# shellcheck disable=SC2139 # no idea why this works but fixing it breaks it
 alias fzfp="fzf $FZF_CTRL_T_OPTS"
 
 # needs findutils for gxargs and fd for better find
 # open a file somewhere under the current directory, press "?" for preview window
+# shellcheck disable=SC2016
 open_fzf() {
   fd -t f -L -H -E ".git" |\
     fzf -m --min-height=20 \
@@ -45,7 +61,7 @@ cd_fzf() {
   local basedir=${1:-.} # default to starting from current directory (.) but allow override
   local directory
   if directory=$(fd -t d -L -H -I -E ".git" . "$basedir" | fzf --preview="tree -L 1 {}" ); then
-    cd $directory && fzf-redraw-prompt
+    cd "${directory}" && fzf-redraw-prompt
   fi
 }
 
