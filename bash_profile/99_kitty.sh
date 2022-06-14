4737ee5d5000b9f3ddf64b9b0fd360c90e143705
Colin Dean <colin.dean@target.com>
2022-06-14 14:04:18 -0400

    Adds a HEJMO_IS_KITTY and a kitty-friendly graph plotter
    `iplot 'x**2'` for an example when running [Kitty][kitty]
    
    [kitty]: https://sw.kovidgoyal.net/kitty

---
+++ bash_profile/99_kitty.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# kitty terminal specific things
+if [ -z "${KITTY_PID}" ]; then
+    # not in kitty
+    return 0
+fi
+export HEJMO_IS_KITTY=1
+
+iplot() {
+cat <<EOF | gnuplot
+    set terminal pngcairo enhanced font 'Fira Sans,10'
+    set autoscale
+    set samples 1000
+    set output '|kitty +kitten icat --stdin yes'
+    set object 1 rectangle from screen 0,0 to screen 1,1 fillcolor rgb"#fdf6e3" behind
+    plot $@
+    set output '/dev/null'
+EOF
+}
