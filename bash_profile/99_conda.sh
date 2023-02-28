5fc921c084ccc053dcc2f5add4f9294622ec97bd
Colin Dean <colin.dean@target.com>
2023-02-28 09:42:46 -0500

    Adds miniconda integration

---
+++ bash_profile/99_conda.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+MINICONDA_PATH="${MINICONDA_PATH:-"$(${BREW_PREFIX})/Caskroom/miniconda"}}"
+CONDA_BASE_PATH="${MINICONDA_PATH}/base"
+CONDA_BIN="${CONDA_BASE_PATH}/bin/conda"
+
+if [[ -x "${CONDA_BIN}" ]]; then
+    __conda_setup="$("${CONDA_BIN}" shell.bash hook 2> /dev/null)"
+    # shellcheck disable=SC2181
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "${CONDA_BASE_PATH}/etc/profile.d/conda.sh" ]; then
+            # shellcheck disable=SC1091
+            . "${CONDA_BASE_PATH}/etc/profile.d/conda.sh"
+        else
+            export PATH="${CONDA_BASE_PATH}/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+fi
