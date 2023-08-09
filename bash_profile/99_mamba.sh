62a6b7e27f5b35ab1aea42783d0f0189d5054950
Colin Dean <colin.dean@target.com>
2023-08-09 11:19:41 -0400

    Adds (micro)mamba loader

---
+++ bash_profile/99_mamba.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba init' !!
+MAMBA_EXE="$(${BREW_PREFIX} micromamba)/bin/micromamba"
+export MAMBA_EXE
+export MAMBA_ROOT_PREFIX="${HOME}/.cache/micromamba"
+if __mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --prefix "$MAMBA_ROOT_PREFIX" 2>/dev/null)"; then
+	#if [ $? -eq 0 ]; then
+	eval "$__mamba_setup"
+else
+	if [ -f "${MAMBA_ROOT_PREFIX}/etc/profile.d/micromamba.sh" ]; then
+		# shellcheck source=/dev/null
+		. "${MAMBA_ROOT_PREFIX}/etc/profile.d/micromamba.sh"
+	else
+		export PATH="${MAMBA_ROOT_PREFIX}/bin:${PATH}" # extra space after export prevents interference from conda init
+	fi
+fi
+unset __mamba_setup
+# <<< mamba initialize <<<
