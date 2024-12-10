22e83feccbaa9deeefb2aefb5e8169b3b07a24f4
Colin Dean <colin.dean@target.com>
2024-12-10 12:58:03 -0500

    Fixes trim function

--- bash_profile/01_strings.sh
+++ bash_profile/01_strings.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 # string manipulation functions
 
+# https://stackoverflow.com/a/3352015
 function trim() {
-	local var=${*}
+	local var="${*}"
+	# read -rd '' var <<<"$var" || :
+	(($#)) || read -re var
 	var="${var#"${var%%[![:space:]]*}"}" # remove leading whitespace characters
 	var="${var%"${var##*[![:space:]]}"}" # remove trailing whitespace characters
 	echo -n "$var"
