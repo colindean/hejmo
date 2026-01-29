823e81f26e48e31cb37982de1c01fcb75adf201a
Colin Dean <colindean@users.noreply.github.com>
2026-01-29 02:09:23 +0000

    Improve Homebrew installer caching and logging messages
    Thanks, shellcheck!

--- _helpers.sh
+++ _helpers.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 command_exists() {
   local cmd="$1"
@@ -8,18 +8,25 @@ command_exists() {
 install_homebrew() {
   local homebrew_installer_url="https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
 
-  command_exists "curl"
-  curl_exists=$?
-  if [[ ${curl_exists} -ne 0 ]]; then
-    if [[ -n "$(command -v "apt")" ]]; then
+  if ! command_exists "curl"; then
+    log_warning "curl is not installed. Attempting to install curl with apt…"
+    if command_exists "apt"; then
         sudo apt install curl
     else
-      echo >&2 "curl isn't available. What's up?"
+      log_failure "apt & curl aren't available. What's up?"
       exit 2
     fi
   fi
 
-  bash -c "$(curl -fsSL "${homebrew_installer_url}")"
+  cache_path="$(mktemp -d)"
+  cached_installer="${cache_path}/install_homebrew.sh"
+
+  log_info "Downloading Homebrew installer to ${cached_installer}…"
+  log_debug "Homebrew installer URL: [${homebrew_installer_url}]"
+  curl -fsSL "${homebrew_installer_url}" > "${cached_installer}"
+
+  log_info "Running Homebrew installer…"
+  bash -c "${cached_installer}"
 }
 
 install_packages() {
@@ -29,11 +36,13 @@ install_packages() {
   IFS=$'\n'
 
   failed=()
+  # TODO: SC2013: Pipe/redirect $listfile to a while read -r loop to avoid subshell issues
   for package in $(cat "${listfile}"); do
-    local clean_package=$(echo "${package}" | sed -e 's/\//\\\//g')
-    local cmd=$(echo "${cmd_template}" | sed -e "s/\%PACKAGE\%/${clean_package}/g")
-    eval "${cmd}"
-    if [[ $? -ne 0 ]]; then
+    local clean_package;
+    clean_package=$(echo "${package}" | sed -e 's/\//\\\//g')
+    local cmd;
+    cmd="${cmd_template//\%PACKAGE\%/${clean_package}}"
+    if ! eval "${cmd}"; then
       failed+=("${package}")
     fi
   done
@@ -60,20 +69,21 @@ link_all_files_in_dir() {
     *) LN_OPTIONS="sf" ;;
   esac
 
+  # TODO: SC2045: Use a loop with globbing instead of ls
   for f in $(ls "${from_dir}"); do
     TARGET="${from_dir}/${f}"
     LINK="${to_dir}/${prepend}${f}"
-    if [[ ! -z ${rm} ]]; then
+    if [[ -n ${rm} ]]; then
       echo "Removing ${LINK}"
       rm -f "${LINK}"
     fi
     echo "Linking ${LINK} to ${TARGET}"
-    ln -sFf "${TARGET}" "${LINK}"
+    ln "${LN_OPTIONS}" "${TARGET}" "${LINK}"
   done
 }
 
 banner_text() {
-  local text="${@}"
+  local text="${*}"
   printf "\033#3%s\n" "${text}"
   printf "\033#4%s\n" "${text}"
   printf "\033#5"
@@ -105,24 +115,24 @@ WARNING_TEXT="${WARNING_COLOR}[${EXCLAMATION} WARN]${RESET_COLOR}"
   DEBUG_TEXT="${DEBUG_COLOR}[${INTERROBANG} DBUG]${RESET_COLOR}"
 
 stderr_log() {
-  echo -e "${@}" >&2
+  echo -e "${*}" >&2
 }
 
 log_success() {
-  stderr_log "${SUCCESS_TEXT} ${BOLD_COLOR}${@}${RESET_COLOR}"
+  stderr_log "${SUCCESS_TEXT} ${BOLD_COLOR}${*}${RESET_COLOR}"
 }
 log_failure() {
-  stderr_log  "${FAILURE_TEXT} ${@}"
+  stderr_log  "${FAILURE_TEXT} ${*}"
 }
 log_info() {
-  stderr_log "${INFO_TEXT} ${@}"
+  stderr_log "${INFO_TEXT} ${*}"
 }
 log_warning() {
-  stderr_log "${WARNING_TEXT} ${@}"
+  stderr_log "${WARNING_TEXT} ${*}"
 }
 log_debug() {
   if [[ -n "${DEBUG}" ]]; then
-    stderr_log "${DEBUG_TEXT} ${@}"
+    stderr_log "${DEBUG_TEXT} ${*}"
   fi
 }
 
