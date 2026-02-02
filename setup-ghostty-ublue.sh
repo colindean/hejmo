b5b42f499797e44d067dad44c02dc454d986a081
Colin Dean <colindean@users.noreply.github.com>
2026-02-01 23:13:44 -0500

    Add setup script for Ghostty install on Bluefin
    This is _just_ stupid enough without the Flatpak for Ghostty available yet.

---
+++ setup-ghostty-ublue.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Set up Ghostty's Fedora COPR repo and install Ghostty via rpm-ostree
+# Author: colindean
+# Adapted from https://ghostty.org/docs/install/binary#atomic-desktops-(silverblue)
+
+set -eux -o pipefail
+
+repo_file="/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:scottames:ghostty.repo"
+
+. /etc/os-release
+
+if [[ "${ID_LIKE}" != fedora ]] || ! command -v rpm-ostree; then
+  echo "This is meant for Universal Blue, which has ID_LIKE=fedora, you've got ${ID_LIKE}, or you're missing rpm-ostree."
+  exit 1
+fi
+
+sudo rm -rf "${repo_file}"
+copr_url="https://copr.fedorainfracloud.org/coprs/scottames/ghostty/repo/fedora-${VERSION_ID}/scottames-ghostty-fedora-${VERSION_ID}.repo"
+curl -fsSL "${copr_url}" | sudo tee "${repo_file}" > /dev/null
+
+rpm-ostree refresh-md && \
+rpm-ostree install ghostty
