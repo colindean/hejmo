91221f78b2370223a06c37ee9ebb5fe523f25158
Colin Dean <git@cad.cx>
2026-06-14 23:11:09 -0400

    Use Homebrew's Rustup if available
    Signed-off-by: Colin Dean <git@cad.cx>

--- bash_profile/99_rust.sh
+++ bash_profile/99_rust.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 # rust management
 
+RUSTUP_BIN="$(brew --prefix rustup)/bin"
+if [[ -d "${RUSTUP_BIN}" ]]; then
+	export PATH="${RUSTUP_BIN}:${PATH}"
+fi
+
 if [[ -f "${HOME}/.cargo/env" ]]; then
 	# shellcheck source=/dev/null
 	source "${HOME}/.cargo/env"
 fi
+
+
